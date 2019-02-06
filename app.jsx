f3ced15e53f8ab6976d015b81f4033fdd714ae1e
codielogan <legendslogu@gmail.com>
2019-02-06 16:54:57 +0530

    first commit have used jsx for components

---
+++ app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';   
+    
+class AppComp extends React.Component { 
+  render(){
+    return <h1> Hello World </h1>	                        
+   }
+}
+export default  AppComp;
--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; 
+// import React from 'react';
+// import ReactDOM from 'react-dom'; 
 
-ReactDOM.render(<h1> Hello World </h1>, document.getElementById('app'));
+// ReactDOM.render(<h1> Hello World </h1>, document.getElementById('app'));
+
+//commented lines are code which doesnt use component structure
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppComp from './App.js';
+
+ReactDOM.render(<AppComp />, document.getElementById('app'));
