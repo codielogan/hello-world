a97cc4b3d1f1f785b1002f78dfb070e27de5c62a
codielogan <47351203+codielogan@users.noreply.github.com>
2019-02-06 18:10:38 +0530

    Update app.jsx
    just for bakti

--- app.jsx
+++ app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
     
 class AppComp extends React.Component { 
   render(){
-    return <h1> Hello World </h1>	                        
+    return <h1> Miss you Bhakti!!! </h1>	                        
    }
 }
 export default  AppComp;
